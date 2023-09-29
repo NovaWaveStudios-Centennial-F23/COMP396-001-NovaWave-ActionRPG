96769b48d42eee251bd729810681f5ceebd44cd6
Sukhmannat Singh <thegamingtitan17@gmail.com>
2023-09-29 13:53:39 -0400

    Ignite basic model done

--- Assets/[Scripts]/Skills/Ignite.cs
+++ Assets/[Scripts]/Skills/Ignite.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Ignite : MonoBehaviour
 {
-    [SerializeField] private IgniteScriptableObject igniteScriptableObejct;
+    public IgniteScriptableObject ignite;
+    public Vector3 direction;
 
+    [SerializeField] private int damage;
+
+    private Rigidbody rb;
+    private float speed = 20f;
+   
     void Start()
     {
-        Debug.Log(igniteScriptableObejct.damage);
+        damage = ignite.damage;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        transform.forward = (direction * 100) - transform.position;
+        rb.velocity = transform.forward * speed;
     }
 }
--- Assets/[Scripts]/Skills/Skills.cs
+++ Assets/[Scripts]/Skills/Skills.cs
@@ -5,18 +5,38 @@ using UnityEngine;
 
 public class Skills : MonoBehaviour
 {
-    [SerializeField] private IgniteScriptableObject ignite;
+    [SerializeField] private IgniteScriptableObject igniteScriptableObject;
+    [SerializeField] private GameObject ignitePrefab;
+    [SerializeField] private LayerMask groundMask;
 
+    private Vector3 mousePosition;
     // Start is called before the first frame update
     void Start()
     {
-        ignite = Resources.Load<IgniteScriptableObject>("Skills/Ignite/Ignite1");
-        Debug.Log(ignite.damage);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundMask))
+        {
+            mousePosition = hit.point;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            InitIgnite();
+        }
+    }
+
+    private void InitIgnite()
+    {
+        igniteScriptableObject = Resources.Load<IgniteScriptableObject>("Skills/Ignite/Ignite2");
+        GameObject ignite = Instantiate(ignitePrefab, transform.position, Quaternion.identity);
+        ignite.GetComponent<Ignite>().ignite = igniteScriptableObject;
+        ignite.GetComponent<Ignite>().direction = mousePosition;
     }
 }
