8e7e0fffdb3b07d91254efa0b42c50e03f30c2ca
Charlie Bi <charlie_bi@hotmail.com>
2023-10-16 22:37:39 -0400

    fixed connector bug

--- Assets/[Scripts]/UI/NodeConnector.cs
+++ Assets/[Scripts]/UI/NodeConnector.cs
@@ -31,8 +31,8 @@ public class NodeConnector : MonoBehaviour
             {
                 for(int j = 0; j < connections.Count; j++)
                 {
-                    Vector3 pos1 = skillNodes[i].gameObject.GetComponent<RectTransform>().position;
-                    Vector3 pos2 = connections[j].gameObject.GetComponent<RectTransform>().position;
+                    Vector3 pos1 = skillNodes[i].gameObject.GetComponent<RectTransform>().localPosition;
+                    Vector3 pos2 = connections[j].gameObject.GetComponent<RectTransform>().localPosition;
                     Connector connection = CreateConnector(pos1, pos2);
                     connection.SetSkillNode(skillNodes[i]);
                 }
@@ -43,8 +43,8 @@ public class NodeConnector : MonoBehaviour
     private Connector CreateConnector(Vector3 pos1, Vector3 pos2)
     {
         //calculate the sides of the triangle
-        float b = pos1.y - pos2.y;
         float a = pos1.x - pos2.x;
+        float b = pos1.y - pos2.y;
         float c = Mathf.Sqrt((b * b) + (a * a));
 
         //calculate the angle
@@ -60,7 +60,7 @@ public class NodeConnector : MonoBehaviour
         //sets the height (length of the line) for the connector
         connector.GetComponent<RectTransform>().localScale = new Vector3(1, c, 1);
         connector.GetComponent<RectTransform>().eulerAngles = new Vector3(0, 0, angleDeg+90);
-        connector.GetComponent<RectTransform>().position = connectorPos;
+        connector.GetComponent<RectTransform>().localPosition = connectorPos;
 
         
 
