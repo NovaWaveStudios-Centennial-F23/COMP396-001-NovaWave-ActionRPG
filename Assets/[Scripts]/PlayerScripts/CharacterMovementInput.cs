5d0f0fe6ca438157d78d281b7e99def5dd8faec7
Mithul <77376534+MithulPy@users.noreply.github.com>
2023-11-10 13:40:23 -0500

    [Add Script to ignore UI layer mask for mouse clicks]

--- Assets/[Scripts]/PlayerScripts/CharacterMovementInput.cs
+++ Assets/[Scripts]/PlayerScripts/CharacterMovementInput.cs
@@ -1,25 +1,38 @@
-// Author: Mithul Koshy
+/**Created by Mithul Koshy
+ * Used to check character movement and mouse clicks in Game
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CharacterMovementInput : MonoBehaviour
 {
     [SerializeField] MouseInput mouseInput;
     CharacterMovement characterMovement;
-    // Start is called before the first frame update
+
+    // Layer mask for the "UI" layer
+    public LayerMask uiLayerMask;
+
     private void Awake()
     {
         characterMovement = GetComponent<CharacterMovement>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            characterMovement.SetDestination(mouseInput.mouseInputPosition);
+            // Perform a raycast to check the layer of the clicked object
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, uiLayerMask))
+            {
+                return;
+            }
+
+            characterMovement.SetDestination(mouseInput.mouseInputPosition);
         }
     }
 }
