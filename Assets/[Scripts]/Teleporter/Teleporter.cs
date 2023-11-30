b8348f29029c17bd2df01a955853a9b9549c87a4
Charlie Bi <charlie_bi@hotmail.com>
2023-11-29 21:58:49 -0500

    added return to town portals

--- Assets/[Scripts]/Teleporter/Teleporter.cs
+++ Assets/[Scripts]/Teleporter/Teleporter.cs
@@ -1,35 +1,6 @@
-using Mirror;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
-
-    //private void GetSceneController()
-    //{
-    //    if(sceneController == null)
-    //    {
-    //    }
-    //}
-
-
-    //public void TeleportLevel1()
-    //{
-
-    //    sceneController.LoadLevelOne();
-    //}
-
-    //public void TeleportLevel2()
-    //{
-    //    sceneController.LoadLevelTwo();
-    //}
-
-    //public void TeleportLevel3()
-    //{
-    //    sceneController.LoadLevelThree();
-    //}
-
-
 
 }
