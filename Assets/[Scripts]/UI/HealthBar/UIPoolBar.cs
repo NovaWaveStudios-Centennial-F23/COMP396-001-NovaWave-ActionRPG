edd26768ac9db6d1b9bb6c16d0092b2db6213f97
Mithul <77376534+MithulPy@users.noreply.github.com>
2023-12-02 02:40:23 -0500

    Updated UIPoolBar (regular update, not displayed on null)

--- Assets/[Scripts]/UI/HealthBar/UIPoolBar.cs
+++ Assets/[Scripts]/UI/HealthBar/UIPoolBar.cs
@@ -21,28 +21,38 @@ public class UIPoolBar : MonoBehaviour
     public void Show(ValuePool targetPool)
     {
         this.targetPool = targetPool;
-        gameObject.SetActive(true);
+        UpdateUI(); // Update UI immediately on show
     }
 
     public void Clear()
     {
         this.targetPool = null;
-        gameObject.SetActive(false);
+        UpdateUI(); // Update UI immediately on clear
     }
 
     private void Update()
     {
+        UpdateUI(); // Regular update of UI
+    }
+
+    private void UpdateUI()
+    {
         if (targetPool != null)
         {
+            if (!gameObject.activeSelf)
+            {
+                gameObject.SetActive(true); // Enable the UI if it's not already enabled
+            }
+
             bar.fillAmount = Mathf.InverseLerp(0f, targetPool.maxValue, targetPool.currentValue);
-            healthIndicator.text = targetPool.currentValue.ToString();
+            healthIndicator.text = targetPool.currentValue.ToString("F0"); // Format for whole numbers, adjust as needed
         }
         else
         {
-            // Handle the case when targetPool is null, e.g., hide the UI elements.
-            bar.fillAmount = 0f;
-            healthIndicator.text = "N/A";
+            if (gameObject.activeSelf)
+            {
+                gameObject.SetActive(false); // Disable the UI if it's currently enabled
+            }
         }
     }
-
 }
