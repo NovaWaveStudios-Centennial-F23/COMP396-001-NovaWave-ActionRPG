677c5ce5e7a1f920acb967cacbc6cb11c402ceea
Sukhmannat Singh <thegamingtitan17@gmail.com>
2023-12-07 14:11:26 -0500

    Save functionality Start

--- Assets/[Scripts]/ScriptableObjects/GearSO.cs
+++ Assets/[Scripts]/ScriptableObjects/GearSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "GearSciptableObject", menuName = "ScriptableObejcts/Create New Gear")]
+[CreateAssetMenu(fileName = "GearSciptableObject", menuName = "ScriptableObejcts/Create New Gear"), System.Serializable]
 public class GearSO : ScriptableObject
 {
     public MeshFilter gearMeshFilter;
